14b25de3f44efd22d138580af236633f7dec3f0b
ClaireDelplancke <cd902@bath.ac.uk>
2022-03-28 15:14:33 +0100

    Add example notebook

---
+++ contrib/plot_results.py
@@ -0,0 +1,10 @@
+# %%
+import cil
+from cil.utilities.display import show2D, show_geometry
+from cil.utilities import noise
+from cil.processors import Slicer
+
+import matplotlib.pyplot as plt
+import numpy as np
+import importlib
+# %%
